f81611d7a27c090799911dc83a7d70184f1aab62
chenhao <c1376479276@163.com>
2021-11-18 09:34:39 +0800

    添加使用示例

---
+++ app/src/main/java/com/lenovo/feizai/optiondialog/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lenovo.feizai.optiondialog;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Author:feizai
+ * Date:2021/11/18-0018 上午 09:20:12
+ * Describe:
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
